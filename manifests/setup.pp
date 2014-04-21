5add12e14caeee0b1e41ca11eb9e0c5fa18f3d09
Lida Tang <lida.tang@gmail.com>
2014-04-21 00:26:48 -0400

    use vcsrepo instead of direct git exec. removed extra setup packages not needed for postgresql

--- manifests/setup.pp
+++ manifests/setup.pp
@@ -1,48 +1,22 @@
 # init -> packages -> user -> setup -> install -> config -> service  
  class gitlab::setup inherits gitlab {
+     
+  vcsrepo {"${gitlab::git_home}/gitlab-shell":
+    provider  => git,
+    ensure    => present,
+    source    => "${gitlab::gitlabshell_sources}",
+    revision  => "${gitlab::gitlabshell_branch}",
+    user     =>  "${gitlab::git_user}"
+  }  
+
+  vcsrepo {"${gitlab::git_home}/gitlab":
+    provider  => git,
+    ensure    => present,
+    source    => "${gitlab::gitlab_sources}",
+    revision  => "${gitlab::gitlab_branch}",
+    user     =>  "${gitlab::git_user}"
+  }  
    
-  # Install bundler gem
-  package { 'bundler':
-    ensure    =>  installed,
-    provider  =>  gem,
-  }
-  
-  package { 'mysql2' :
-    ensure    =>  '0.3.11',
-    provider  =>  gem,
-  }
-  
-  # Install charlock_holmes 
-  package { 'charlock_holmes':
-    ensure    =>  '0.6.9.4',
-    provider  =>  'gem',
-    require   =>  [
-            Package['bundler'],
-            Package['mysql2'],
-                  ],
-                  # TODO: Add notify to notify()
-  }
-  
-  # Execute all commands as the git user in the git home directory
-  Exec {
-    user    =>  "${gitlab::git_user}",
-    cwd     =>  "${gitlab::git_home}",
-    path    =>  '/usr/bin',
-  }
-  
-  # Download gitlab-shell (replaces git-o-lite)
-  exec { 'clone branch gitlab-shell':
-    command   =>  "/usr/bin/git clone ${gitlab::gitlabshell_sources} -b ${gitlab::gitlabshell_branch} ${gitlab::git_home}/gitlab-shell",
-    creates   =>  "${gitlab::git_home}/gitlab-shell",
-  }
-  
-  # Download gitlab source
-  exec { 'download gitlab':
-    command   =>  "/usr/bin/git clone -b ${gitlab::gitlab_branch} ${gitlab::gitlab_sources} ${gitlab::git_home}/gitlab",
-    creates   =>  "${gitlab::git_home}/gitlab",
-  }
-  
-  
   # Copy the gitlab-shell config
   file { "${gitlab::git_home}/gitlab-shell/config.yml":
     ensure    =>  file,
@@ -50,8 +24,8 @@
     owner     =>  "${gitlab::git_user}",
     group     =>  'git',
     require   =>  [
-              Exec['clone branch gitlab-shell'],
-              Exec['download gitlab'],
+              Vcsrepo["${gitlab::git_home}/gitlab-shell"],
+              Vcsrepo["${gitlab::git_home}/gitlab"],
                   ],
   }
   
@@ -63,8 +37,8 @@
     owner     =>  "${gitlab::git_user}",
     group     =>  'git',  
     require   =>  [
-              Exec['clone branch gitlab-shell'],
-              Exec['download gitlab'],
+              Vcsrepo["${gitlab::git_home}/gitlab-shell"],
+              Vcsrepo["${gitlab::git_home}/gitlab"],
                   ],
   }
   
@@ -75,8 +49,8 @@
     owner     =>  "${gitlab::git_user}",
     group     =>  'git',
     require   =>  [
-              Exec['clone branch gitlab-shell'],
-              Exec['download gitlab'],
+              Vcsrepo["${gitlab::git_home}/gitlab-shell"],
+              Vcsrepo["${gitlab::git_home}/gitlab"],
                   ],
   }
   
@@ -88,8 +62,8 @@
     group     =>  'git',
     mode      =>  '0640',
     require   =>  [
-              Exec['clone branch gitlab-shell'],
-              Exec['download gitlab'],
+              Vcsrepo["${gitlab::git_home}/gitlab-shell"],
+              Vcsrepo["${gitlab::git_home}/gitlab"],
                   ],
   }
 
